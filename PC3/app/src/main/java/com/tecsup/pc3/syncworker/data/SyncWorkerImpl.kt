50e2cefb9be66d5780204019d77c47a724ee1916
yuri <aqpyuri20081@gmail.com>
2026-06-07 23:21:36 -0500

    feat(sync-worker): implementa SyncWorkerImpl scaffold

---
+++ PC3/app/src/main/java/com/tecsup/pc3/syncworker/data/SyncWorkerImpl.kt
@@ -0,0 +1,30 @@
+package com.tecsup.pc3.syncworker.data
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+// import dagger.assisted.Assisted
+// import dagger.assisted.AssistedInject
+import kotlinx.coroutines.flow.first
+
+// @HiltWorker
+class SyncWorkerImpl /* @AssistedInject */ constructor(
+    /* @Assisted */ context: Context,
+    /* @Assisted */ params: WorkerParameters,
+    // private val connectivityMonitor: ConnectivityMonitor,
+    // private val tripRepository: TripRepository
+) : CoroutineWorker(context, params) {
+
+    override suspend fun doWork(): Result {
+        // Simulación temporal para que no marque error hasta que tengas los repositorios
+        val isConnected = true // connectivityMonitor.isConnected.first()
+        if (!isConnected) return Result.success()
+
+        val tripStatus = "in_progress" // tripRepository.getActiveTrip()?.status
+        if (tripStatus != "in_progress" && tripStatus != "pending_end") {
+            return Result.success()
+        }
+
+        return Result.success()
+    }
+}
