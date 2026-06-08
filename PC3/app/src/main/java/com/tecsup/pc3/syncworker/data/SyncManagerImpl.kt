b9645c6a908ce4bfdf9f763387eaf436d8b8c92a
yuri <aqpyuri20081@gmail.com>
2026-06-07 23:21:09 -0500

    feat(sync-worker): implementa SyncManagerImpl

---
+++ PC3/app/src/main/java/com/tecsup/pc3/syncworker/data/SyncManagerImpl.kt
@@ -0,0 +1,31 @@
+package com.tecsup.pc3.syncworker.data
+
+import androidx.work.*
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+
+class SyncManagerImpl @Inject constructor(
+    private val workManager: WorkManager
+) /* : SyncManager */ { // Descomenta la interfaz cuando la tengas
+
+    fun enqueuePeriodicSync() { // override
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val request = PeriodicWorkRequestBuilder<SyncWorkerImpl>(15, TimeUnit.MINUTES)
+            .setConstraints(constraints)
+            .build()
+
+        workManager.enqueueUniquePeriodicWork(
+            "SyncWorker",
+            ExistingPeriodicWorkPolicy.KEEP,
+            request
+        )
+    }
+
+    fun enqueueImmediateSync() { // override
+        val request = OneTimeWorkRequestBuilder<SyncWorkerImpl>().build()
+        workManager.enqueue(request)
+    }
+}
