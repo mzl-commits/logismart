408d7569e06b8a6b0e0dc9f5d67f6f2f4721a3df
giselamorales-oss <gisela.morales@tecsup.edu.pe>
2026-06-27 20:16:59 -0500

    chore(ui-components): organizar theme colors y typography en archivo compartido

--- logismart-movil/app/src/main/java/com/tecsup/logismart_movil/ui/theme/Color.kt
+++ logismart-movil/app/src/main/java/com/tecsup/logismart_movil/ui/theme/Color.kt
@@ -6,6 +6,18 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
+val Purple40 = Color(0xFF6650A4)
+val PurpleGrey40 = Color(0xFF625B71)
 val Pink40 = Color(0xFF7D5260)
+
+val PrimaryBlue = Color(0xFF1565C0)
+val PrimaryDarkBlue = Color(0xFF0D47A1)
+val SecondaryBlue = Color(0xFF42A5F5)
+
+val SuccessGreen = Color(0xFF2E7D32)
+val WarningOrange = Color(0xFFF57C00)
+val ErrorRed = Color(0xFFC62828)
+
+val BackgroundLight = Color(0xFFF8FAFC)
+val SurfaceLight = Color(0xFFFFFFFF)
+val TextDark = Color(0xFF1F2937)
