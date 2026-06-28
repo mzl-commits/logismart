83233bcfda8f1e982af48e30bb822a6edf788733
yuri <aqpyuri20081@gmail.com>
2026-06-28 16:35:10 -0500

    Security: add MobileTokenAuthentication to REST_FRAMEWORK DEFAULT_AUTHENTICATION_CLASSES

--- almacen_config/settings.py
+++ almacen_config/settings.py
@@ -148,6 +148,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'clasificacion.views_mobile.MobileTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
 }
 
 
